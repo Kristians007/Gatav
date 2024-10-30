c25c6f6902eddbb9b2da2c06966c68df32e7f6b3
Kristians007 <kristians.timofejevs3@gmail.com>
2024-10-30 15:13:21 +0200

    Update uzdevums.py

--- uzdevums.py
+++ uzdevums.py
@@ -1,1 +1,11 @@
-print("Hello")
+guesswho = 4
+i = 0
+while i < 1:
+    vert = float(input("uzmini skaitli"))
+    if vert == guesswho:
+        print("uzmineji")
+        i = 1
+    elif vert > guesswho:
+        print("mazaks")
+    else:
+            print("lielaks")
